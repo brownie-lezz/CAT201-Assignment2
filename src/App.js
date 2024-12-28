65170257a341a59b73424df279960d48f3f1d895
brownie-lezz <shirleyjong2@gmail.com>
2024-12-28 19:59:57 +0800

    Navigation bar js and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route>
+          <Route path='/' exact />
+        </Route>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import './Navbar.css';
+import {Button} from './Button';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMenu = () => setClick(false);
+  const [button, setButton] = useState(true);
+  const showButton = () => {
+    if(window.innerWidth <= 960){
+      setButton(false);
+    } else{
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMenu}>
+            PenGems
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className = 'nav-item'>
+              <Link to='/culture-heritage' className='nav-links' onClick={closeMenu}>
+                Heritage
+              </Link>
+            </li>
+            <li className = 'nav-item'>
+              <Link to='/nature-wildlife' className='nav-links' onClick={closeMenu}>
+                Nature
+              </Link>
+            </li>
+            <li className = 'nav-item'>
+              <Link to='/food-beverages' className='nav-links' onClick={closeMenu}>
+                Food and Beverages
+              </Link>
+            </li>
+            <li className = 'nav-item'>
+              <Link to='/accomodations' className='nav-links' onClick={closeMenu}>
+                Accomodations
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
